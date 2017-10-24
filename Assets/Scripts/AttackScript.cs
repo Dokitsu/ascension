6e9121008126751fbb6ca9bf520caad6dacd1aec
Dokitsu <projectfuzzle@gmail.com>
2017-10-24 16:24:55 +0100

    Attackscript
    Added attack script

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour {
+
+    public GameObject Attacker;
+    public GameObject Target;
+    public Vector3 Atkcoor;
+    public Vector3 Tarcoor;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void AttackerRange()
+    {
+
+    }
+
+    void AttackerDam()
+    {
+
+    }
+
+    void DefRoll()
+    {
+
+    }
+}
