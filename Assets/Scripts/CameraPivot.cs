bf5020be930b1f458443524e1ce5ae09d329cddb
Matthew Stageman <mattstageman@gmail.com>
2017-10-24 21:05:11 +0100

    Title Screen terrain
    Added a terrain and a rotaion script for the camera.

---
+++ Assets/Scripts/CameraPivot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPivot : MonoBehaviour
+{
+    public int SpeedOfCamera;
+
+	void Update ()
+    {
+        transform.Rotate(0, SpeedOfCamera*Time.deltaTime, 0);
+	}
+}
