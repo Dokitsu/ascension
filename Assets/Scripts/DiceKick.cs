b0f294d17ae4d9b92cd6f495fdea4261a47b3d07
Dokitsu <projectfuzzle@gmail.com>
2017-10-28 18:05:28 +0100

    Dice
    Added Dice and dice roll script

---
+++ Assets/Scripts/DiceKick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceKick : MonoBehaviour {
+
+    public string buttonName = "Fire1";
+    public float force = 10.0f;
+    public ForceMode forceMode;
+    public float angforce;
+
+    //public GameObject die;
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown(buttonName))
+        {
+            KickDie();
+        }
+	}
+
+    void KickDie()
+    {
+        Rigidbody die = GetComponent<Rigidbody>();
+        die.AddForce(Random.onUnitSphere * force, forceMode);
+        die.AddTorque(Random.onUnitSphere * angforce, forceMode);
+    }
+}
