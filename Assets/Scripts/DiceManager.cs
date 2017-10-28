a58eb18ed76c2c7cad97fa58bf26ed82e7edc741
Dokitsu <projectfuzzle@gmail.com>
2017-10-28 19:20:52 +0100

    Dice
    Dice rolls and reads number rolled

--- Assets/Scripts/DiceManager.cs
+++ Assets/Scripts/DiceManager.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class DiceManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public LayerMask diecoll = -1;
+
+    public int value = 1;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        RaycastHit hit;
+		if(Physics.Raycast(transform.position,Vector3.up,out hit, Mathf.Infinity, diecoll))
+        {
+            value = hit.collider.GetComponent<DiceVal>().value;
+        }
 	}
+
+    void OnGUI()
+    {
+        GUILayout.Label(value.ToString());
+    }
 }
