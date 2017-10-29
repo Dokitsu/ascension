6cea554f2b250c72e1b8c0c35d63c9191ab15796
Dokitsu <projectfuzzle@gmail.com>
2017-10-29 01:18:58 +0000

    Title
    Added audio
    Camera speed variance
    skybox changes

--- Assets/Scripts/CameraPivot.cs
+++ Assets/Scripts/CameraPivot.cs
@@ -6,8 +6,25 @@ public class CameraPivot : MonoBehaviour
 {
     public int SpeedOfCamera;
 
+    void Start()
+    {
+        StartCoroutine(speedswitch());
+    }
+
 	void Update ()
     {
         transform.Rotate(0, SpeedOfCamera*Time.deltaTime, 0);
 	}
+
+    public IEnumerator speedswitch()
+    {
+        while (true)
+        {
+            SpeedOfCamera = Random.Range(2, 6);
+            yield return new WaitForSeconds(2f);
+            SpeedOfCamera = 4;
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
 }
