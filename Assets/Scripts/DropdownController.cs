80ac3d46b63577f08623bf26e4e548a7ab3a5430
Matthew Stageman <mattstageman@gmail.com>
2017-11-19 15:39:19 +0000

    Title screen dropdowns
    Made it so that when you have one aliie it only shows one dropdown box for the one allie.  if you have more, it will acommpany that .

---
+++ Assets/Scripts/DropdownController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DropdownController : MonoBehaviour
+{
+    public Dropdown NPlayer;
+
+    public GameObject Dropwdown1;
+    public GameObject Dropwdown2;
+    public GameObject Dropwdown3;
+    public GameObject Dropwdown4;
+
+    public int nPlayer;
+
+
+    void Start()
+    {
+       
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        nPlayer = NPlayer.value;
+        if (NPlayer.value == 0)
+        {
+            Dropwdown1.SetActive(true);
+            Dropwdown2.SetActive(false);
+            Dropwdown3.SetActive(false);
+            Dropwdown4.SetActive(false);
+        }
+        if (NPlayer.value == 1)
+        {
+            Dropwdown1.SetActive(true);
+            Dropwdown2.SetActive(true);
+            Dropwdown3.SetActive(false);
+            Dropwdown4.SetActive(false);
+        }
+        if (NPlayer.value == 2)
+        {
+            Dropwdown1.SetActive(true);
+            Dropwdown2.SetActive(true);
+            Dropwdown3.SetActive(true);
+            Dropwdown4.SetActive(false);
+        }
+        if (NPlayer.value == 3)
+        {
+            Dropwdown1.SetActive(true);
+            Dropwdown2.SetActive(true);
+            Dropwdown3.SetActive(true);
+            Dropwdown4.SetActive(true);
+        }
+    }
+
+}
